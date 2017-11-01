686cb0868425e40d489ca8531ccaf0aa5c9d7085
Tyler Thomson <tylerthomson@protonmail.com>
2017-11-01 12:37:31 -0500

    Added custom user model identifier in views.py

--- apps/api/views.py
+++ apps/api/views.py
@@ -2,6 +2,9 @@ from __future__ import unicode_literals
 from rest_framework import permissions, viewsets
 from .serializers import *
 
+from django.contrib.auth import get_user_model #Required b/c custom User model
+User = get_user_model()#Required b/c custom User model
+
 class UserViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited
