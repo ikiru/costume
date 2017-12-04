1b443e7f508bac56e28195c1ec258ff0ebaeecf7
Jeff Winkler <jeff.a.winkler@gmail.com>
2017-12-03 18:23:17 -0600

    added component skeletons to the Portal section

---
+++ frontend/src/components/Portal/Portalmenu/Portalmenu.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Portalmenu.css';
+
+const portalmenu= (props) => {
+    return(
+        <div className=Portalmenu>
+
+        </div>
+    )
+}
+
+
+export default portalmenu;
---
+++ frontend/src/components/Portal/Search/Search.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Search.css';
+
+const search = (props) => {
+    return(
+        <div className=Search>
+
+        </div>
+    )
+}
+
+
+export default search;
