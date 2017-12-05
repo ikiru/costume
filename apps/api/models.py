d5887fc17d7d92b63b84608f13ace566e9260674
Tyler Thomson <tylerthomson@protonmail.com>
2017-12-05 14:44:56 -0600

    Completed default user model

--- apps/api/models.py
+++ apps/api/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
 from django.db import models
+from __future__ import unicode_literals
+from django.contrib.auth.models import User
 
     # def __str__(self):
     #     string_output = " ID: {} Email: {} Password: {} Admin: {}"
--- apps/api/views.py
+++ apps/api/views.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 from rest_framework import permissions, viewsets
 from .serializers import *
+from django.contrib.auth.models import User
 
 class UserViewSet(viewsets.ModelViewSet):
     """
