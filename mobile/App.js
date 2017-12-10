56223873179a5180b77c6d88ad509195beee2767
Lizhang_Xie <lizhang.xie@gmail.com>
2017-12-10 15:19:41 -0600

    added basic layout for home page /w button tag

--- mobile/App.js
+++ mobile/App.js
@@ -9,29 +9,64 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  Button
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+// const instructions = Platform.select({
+//   ios: 'Press Cmd+R to reload,\n' +
+//     'Cmd+D or shake for dev menu',
+//   android: 'Double tap R on your keyboard to reload,\n' +
+//     'Shake or press menu button for dev menu',
+// });
+
+const onPressLearnMore = () => {
+    console.log('pressed');
+};
 
 export default class App extends Component<{}> {
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome to Better Off Costumes!
         </Text>
-        <Text style={styles.instructions}>
+        {/* <Text style={styles.instructions}>
           To get started, edit App.js
         </Text>
         <Text style={styles.instructions}>
           {instructions}
-        </Text>
+        </Text> */}
+        <Button 
+            onPress={onPressLearnMore}
+            title="Settings"
+            color="#841584"
+            accessibilityLabel="Learn more about this purple button"
+        />
+        <Button 
+            onPress={onPressLearnMore}
+            title="New Costume"
+            color="#841584"
+            accessibilityLabel="Learn more about this purple button"
+        />
+        <Button 
+            onPress={onPressLearnMore}
+            title="Search"
+            color="#841584"
+            accessibilityLabel="Learn more about this purple button"
+        />
+        <Button 
+            onPress={onPressLearnMore}
+            title="Check In"
+            color="#841584"
+            accessibilityLabel="Learn more about this purple button"
+        />
+        <Button 
+            onPress={onPressLearnMore}
+            title="Check Out"
+            color="#841584"
+            accessibilityLabel="Learn more about this purple button"
+        />
       </View>
     );
   }
