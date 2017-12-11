50eb33f28889fda024cd915e9d89a6b9b6066ea2
Tyler Thomson <tylerthomson@protonmail.com>
2017-12-10 19:47:26 -0600

    Got token authentication going again

--- server/apps/api/serializers.py
+++ server/apps/api/serializers.py
@@ -2,7 +2,7 @@ from models import *
 from rest_framework import serializers
 from django.contrib.auth.models import User, Group
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
