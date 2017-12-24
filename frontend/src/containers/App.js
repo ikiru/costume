3aff904af7dc9aa0013e316850e4554583587a8c
Jeff Winkler <jeff.a.winkler@gmail.com>
2017-12-23 23:05:34 -0600

    added containers and components to correct directories,  Started work on dynamic forms

---
+++ frontend/src/containers/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import Login from '../components/login/Login'
+import Menu from '../components/Menu/Menu';
+import Header from '../components/Header/Header';
+import Footer from '../components/Footer/Footer';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+      </div>
+      
+    );
+  }
+}
+
+export default App;
